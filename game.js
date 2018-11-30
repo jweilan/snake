5de1ebf7ab6fffb25dbca587328d6f805b92eb78
jweilan <1123217106@qq.com>
2018-11-30 21:42:17 +0800

    贪吃蛇demo版本

---
+++ game.js
@@ -0,0 +1,4 @@
+require("weapp-adapter1e13299d.js");
+require("libs/laya.wxminidb8c6c12.js");
+window.loadLib = require;
+require("index6195657f.js")
---
+++ index6195657f.js
@@ -0,0 +1,16 @@
+/**
+ * 设置LayaNative屏幕方向，可设置以下值
+ * landscape           横屏
+ * portrait            竖屏
+ * sensor_landscape    横屏(双方向)
+ * sensor_portrait     竖屏(双方向)
+ */
+window.screenOrientation = "sensor_landscape";
+
+//-----libs-begin-----
+loadLib("libs/laya.coref31e9aeb.js")
+loadLib("libs/laya.webglcdfbdd24.js")
+loadLib("libs/laya.uife0eafe4.js")
+loadLib("libs/laya.physicsce764716.js")
+//-----libs-end-------
+loadLib("js/bundle75673ece.js");
